f62cfb623d462f98e42120c8d205d8e7f29bdbf1
fadimata tillman <fadi.tillman@gmail.com>
2020-01-22 05:27:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,12 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-end
+
 
 
 
 #introduction_with_language
 def introduction_with_language(name, language)
-  puts "Hi,my name is #{name} I'am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am
+  learning to program in #{language}."
 end
